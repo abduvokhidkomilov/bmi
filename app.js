60c815f367316a6716c7510d70b74081caf170a6
abduvokhidkomilov <abduvokhidkomilo0710@gmail.com>
2022-12-13 23:50:03 +0500

    nwe

--- app.js
+++ app.js
@@ -14,37 +14,40 @@ let bharf=ism.charAt(0).toUpperCase()
 let davomi=ism.slice(1).toLowerCase()
 let result=bharf+davomi
 let bmi = vazn/boy**2
+one.style.display='none'
+two.style.display='block'
 if  (bmi<18.5) {
     p.textContent=`${result} siz juda ozg'insiz😫`
-    img.src="https://b7.pngbarn.com/png/311/102/cartoon-graphy-drawing-thin-png-clip-art.png"
     document.body.style.backgroundColor="orangered"
-    img.width="400px"
-    img.height="400px"
+    img.src="https://www.clipartmax.com/png/full/63-632264_thin-girl-clipart-black-and-white-skinny-girl-clipart-transparent.png"
+    img.style.width="100%"
+    img.style.height="100%"
+    two.style.backgroundColor='orangered'
 }
 
 else if (bmi>18.5 && bmi<=24.9) {
 p.textContent=` ${result} siz normal vaznli insonsiz😊`
-img.src="https://img1.pnghut.com/20/2/11/0CyGQeNCF5/cartoon-scalable-vector-graphics-tuxedo-businessperson-business.jpg"
+img.src="./images/norml.png"
 document.body.style.backgroundColor="orangered"
-img.width="400px"
-img.height="400px"
+img.width="100%"
+img.height="100%"
 
 
 }
 else if (bmi>25 && bmi<=29.9) {
     p.textContent=`${result} siz ortiqcha vaznli insonsiz😮`
-    img.src="https://yandex.ru/images/search?pos=13&from=tabbar&img_url=http%3A%2F%2Fw7.pngwing.com%2Fpngs%2F352%2F232%2Fpng-transparent-the-fat-man-fat-overweight.png&text=plump+person+png&rpt=simage&lr=10336"
+    img.src="./images/plump.png"
     document.body.style.backgroundColor="orangered"
-    img.width="400px"
-    img.height="400px"
+    img.width="100%"
+    img.height="100%"
     }
 
 else if (bmi>30) {
         p.textContent=`${result} siz semirib ketgansiz☹`       
-        img.src="https://yandex.ru/images/search?p=2&text=plump+person+icon&pos=88&rpt=simage&img_url=http%3A%2F%2Fe7.pngegg.com%2Fpngimages%2F888%2F72%2Fpng-clipart-barbell-mime-barbell-fat-material-png-material-child.png&from=tabbar&lr=10336"
+        img.src="./images/plump.png"
         document.body.style.backgroundColor="orangered"
-        img.width="400px"
-        img.height="400px"
+        img.width="100%"
+        img.height="100%"
     }
        
 
